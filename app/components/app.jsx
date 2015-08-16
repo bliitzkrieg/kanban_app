c08eda33b4f96b194ab670cd1bd8a1bd536dce36
Luca D <lucamatthewdecaprio@gmail.com>
2015-08-16 00:25:22 -0400

    Added notes and note components

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,8 +1,65 @@
+import uuid from 'node-uuid';
+import 'core-js/fn/array/find-index';
 import React from 'react';
-import Note from './Note';
+import Notes from './Notes';
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            notes: [
+                {
+                    id: uuid.v4(),
+                    task: 'Learn webpack!'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Learn React'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Cook Dinner'
+                }
+            ]
+        };
+
+        this.addItem = this.addItem.bind(this);
+        this.itemEdited = this.itemEdited.bind(this);
+    }
     render() {
-        return <Note />;
+        const notes = this.state.notes;
+
+        return (
+            <div>
+                <button onClick ={this.addItem}>+</button>
+                <Notes items={notes} onEdit={this.itemEdited} />
+            </div>
+        );
+    }
+    addItem() {
+        this.setState({
+            notes: this.state.notes.concat([{
+                id: uuid.v4(),
+                task: "New task"
+            }])
+        });
+    }
+    itemEdited(noteId, task) {
+        let notes = this.state.notes;
+        const noteIndex = notes.findIndex((note) => note.id === noteId);
+
+        if(noteId < 0){
+            return console.warn('Failed to find note', note, noteId);
+        }
+
+        if(task) {
+            notes[noteIndex].task = task;
+        }
+        else{
+            notes = notes.slice(0, noteIndex).concat(notes.slice(noteIndex + 1));
+        }
+
+        this.setState({notes});
     }
 }
